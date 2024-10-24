d5bc373ab889852e251f3a2412c5988e0a298f76
JoeGuillory <joeguillory15@gmail.com>
2024-10-23 21:06:49 -0500

    changes

--- PersonalGameProject/Game.cs
+++ PersonalGameProject/Game.cs
@@ -1,5 +1,6 @@
 ﻿using Raylib_cs;
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using MathLibrary;
@@ -13,28 +14,30 @@ namespace PersonalGameProject
 {
     internal  class Game
     {
-        Player player = new Player();
-        Model pistol = new Model();
-        Texture2D pistolTexture = new Texture2D();
-        Model sniper = new Model();
-        Texture2D sniperTexture = new Texture2D();
         
+        
+        Player player = new Player();
+        Model barrel = new Model();
+        Texture2D barrelpng = new Texture2D();
+        Texture2D colorpallet = new Texture2D();
+        Model floor = new Model();
         
 
         public void Start()
         {
+          
             Raylib.SetTargetFPS(144);
             Raylib.DisableCursor();
             player.InitCamera();
-            //Need to change file loacation sorry i dont know another good way of doing this
-            sniper = Raylib.LoadModel(@"C:\Dev\GitFolder\PersonalRaylibProject\Models\Sniper\sniper.obj");
-            sniperTexture = Raylib.LoadTexture(@"C:\Dev\GitFolder\PersonalRaylibProject\Models\Sniper\sniper.png");
-            pistol = Raylib.LoadModel(@"C:\Dev\GitFolder\PersonalRaylibProject\Models\Pistol\pistol.obj");
-            pistolTexture = Raylib.LoadTexture(@"C:\Dev\GitFolder\PersonalRaylibProject\Models\Pistol\pistol.png");
 
-            Console.WriteLine(pistol.MaterialCount);
-            Raylib.SetMaterialTexture(ref pistol, 2, MaterialMapIndex.Brdf, ref pistolTexture);
-            Raylib.SetMaterialTexture(ref sniper, 2, MaterialMapIndex.Albedo, ref sniperTexture);
+            floor = Raylib.LoadModel(@"res\Models\floor.obj");
+            barrel = Raylib.LoadModel(@"res\Models\barrel.obj");
+            colorpallet = Raylib.LoadTexture(@"res\Models\colormap.png");
+            Console.WriteLine(floor.MaterialCount);
+
+           Raylib.SetMaterialTexture(ref barrel, 0,MaterialMapIndex.Albedo, ref colorpallet);
+           Raylib.SetMaterialTexture(ref floor, 0,MaterialMapIndex.Albedo, ref colorpallet);
+            
             
             
             
@@ -48,20 +51,17 @@ namespace PersonalGameProject
 
             player.Movement();
             Raylib.BeginMode3D(player.playerCamera);
-            Raylib.DrawModel(pistol, (Vector3)player.playerCamera.Position + new Vector3(.5f,0,0), 10.0f, Color.White);
+            
+            Raylib.DrawModel(floor, new Vector3(0.02f, 0.02f, 0), 2, Color.White);
             
             player.Draw();
             Raylib.DrawGrid(200, 1);
-            Raylib.DrawPlane(new Vector3(0.0f, 0.0f, 0.0f),new Vector2(32,32), Color.LightGray); // Draw ground
-            Raylib.DrawCube(new Vector3( -16.5f, 2.5f, 0.0f), 1.0f, 5.0f, 32.0f, Color.Blue);     // Draw a blue wall
-            Raylib.DrawCube(new Vector3(16.5f, 2.5f, 0.0f), 1.0f, 5.0f, 32.0f, Color.Lime);      // Draw a green wall
-            Raylib.DrawCube(new Vector3(0.0f, 2.5f, 16.5f), 32.0f, 5.0f, 1.0f, Color.Gold); // Draw a gold wall
-            Raylib.DrawCube(new Vector3(0.0f, 2.5f, -16.5f), 32.0f, 5.0f, 1.0f, Color.Gold); // Oposite Gold wall
+           
             
             
 
             Raylib.EndMode3D();
-            Raylib.DrawText("Position: " + player.playerCamera.Position, 10, 10, 10,Color.White);
+            
             
         }
         public void End()
